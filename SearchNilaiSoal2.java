cdbbaff6042c660697fead3a36988678758799ed
unknown <farrelandika14@gmail.com>
2024-10-31 01:38:27 +0800

    pencarian soal 2

---
+++ SearchNilaiSoal2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class SearchNilaiSoal2 {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int n = sc.nextInt();
+
+        int[] Nilai = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            Nilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
+
+        int hasil = -1;
+        for (int i = 0; i < Nilai.length; i++) {
+            if (key == Nilai[i]) {
+                hasil = i;
+                break;
+            }
+        }
+
+        System.out.println();
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
+        }else {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+
+        }
+    }
+}
